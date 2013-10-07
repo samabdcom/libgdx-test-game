b82a35d76a1abe33528f22e737e60e0827ee01fa
Sam Sky <samsky@outlook.com>
2013-10-07 15:57:11 +0400

    packed all images into texture

---
+++ my-test-game/src/com/me/mygdxgame/utils/TextureSetup.java
@@ -0,0 +1,12 @@
+// It's generate textures
+/*
+package com.me.mygdxgame.utils;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+
+public class TextureSetup {
+
+	public static void main(String[] args) {
+		TexturePacker2.process("D:\\Workspace\\games\\libgdx\\projects\\libgdx-test-game\\my-test-game-android\\assets\\data\\images\\", "D:\\Workspace\\games\\libgdx\\projects\\libgdx-test-game\\my-test-game-android\\assets\\data\\images\\textures\\", "textures.pack");
+	}
+}
+*/
